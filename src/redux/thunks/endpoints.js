404e4619b0df7adbe8754d31aaac8c87f87070c3
Gonzagadavid <gonzagadaviddev@gmail.com>
2022-02-17 10:09:28 -0300

    atualizado endpoints de acordo com o deploy

--- src/redux/thunks/endpoints.js
+++ src/redux/thunks/endpoints.js
@@ -1,7 +1,7 @@
-export const LOGIN = 'http://localhost:3800/user/login';
+export const LOGIN = 'https://todo-priority-list.herokuapp.com/user/login';
 
-export const SIGNUP = 'http://localhost:3800/user';
+export const SIGNUP = 'https://todo-priority-list.herokuapp.com/user';
 
-export const GET_TASKS = 'http://localhost:3800/task';
+export const GET_TASKS = 'https://todo-priority-list.herokuapp.com/task';
 
-export const TASK_BY_ID = (id) => `http://localhost:3800/task/${id}`;
+export const TASK_BY_ID = (id) => `https://todo-priority-list.herokuapp.com/task/${id}`;
