04fdff2ef691685cb719f74cd5b916b2ba3f2131
gonzagadavid <david.blesses@hotmail.com>
2022-04-05 21:31:45 -0300

    test: desenvolvido testes de integração para a rota DELETE /task/:id

--- src/test/deleteTask.test.js
+++ src/test/deleteTask.test.js
@@ -1,14 +1,15 @@
 import { MongoClient } from 'mongodb';
 import {
-  describe, expect, it, jest, beforeAll, afterAll,
+  describe, jest, beforeAll, afterAll, expect, it,
 } from '@jest/globals';
 import request from 'supertest';
 import MongoClientMock from './helpers/MongoClientMock';
 import app from '../api/app';
 
-describe('Get /task', () => {
+describe('Get /task/:id', () => {
   let authorization = null;
   let connectionMock = null;
+  let taskId = null;
   beforeAll(async () => {
     connectionMock = await MongoClientMock();
     MongoClient.connect = jest.fn().mockResolvedValueOnce(connectionMock);
@@ -40,48 +41,37 @@ describe('Get /task', () => {
       .post('/task')
       .set({ authorization })
       .send({
-        title: 'Tarefa 1',
-        description: 'Descrição da tarefa 1',
+        title: 'Tarefa teste',
+        description: 'Descrição da tarefa teste',
         priority: '2',
         status: 'inProcess',
       });
 
-    await req
-      .post('/task')
-      .set({ authorization })
-      .send({
-        title: 'Tarefa 2',
-        description: 'Descrição da tarefa 2',
-        priority: '2',
-        status: 'inProcess',
-      });
+    const { _id } = await tasks.findOne({ title: 'Tarefa teste' });
+    taskId = _id.toString();
   });
 
   afterAll(() => {
     jest.restoreAllMocks();
   });
 
-  it('verifica a resposta ao tentar buscar tasks sem o token', async () => {
+  it('verifica a resposta ao tentar deletar uma task sem o token', async () => {
     const req = request(app);
-    const response = await req.get('/task');
+    const response = await req.delete(`/task/${taskId}`);
 
     expect(response.statusCode).toBe(401);
     expect(response.body).toHaveProperty('message');
     expect(response.body.message).toBe('missing auth token');
   });
 
-  it('verifica a resposta ao tentar buscar tasks corretamente', async () => {
+  it('verifica a resposta ao tentar deletar uma task com sucesso', async () => {
     const req = request(app);
     const response = await req
-      .get('/task')
+      .delete(`/task/${taskId}`)
       .set({ authorization });
 
-    response.body.forEach((task) => {
-      expect(task).toHaveProperty('title');
-      expect(task).toHaveProperty('status');
-      expect(task).toHaveProperty('priority');
-      expect(task).toHaveProperty('created');
-      expect(task).toHaveProperty('_id');
-    });
+    expect(response.statusCode).toBe(202);
+    expect(response.body).toHaveProperty('message');
+    expect(response.body.message).toBe('task removed successfully');
   });
 });
--- src/test/putTask.test.js
+++ src/test/putTask.test.js
@@ -1,21 +1,24 @@
 import { MongoClient } from 'mongodb';
 import {
-  describe, expect, it, jest, beforeAll, afterAll,
+  describe, jest, beforeAll, afterAll, expect, it,
 } from '@jest/globals';
 import request from 'supertest';
 import MongoClientMock from './helpers/MongoClientMock';
 import app from '../api/app';
 
-describe('Post /task', () => {
+describe('Put /task/:id', () => {
   let authorization = null;
   let connectionMock = null;
+  let taskId = null;
   beforeAll(async () => {
     connectionMock = await MongoClientMock();
     MongoClient.connect = jest.fn().mockResolvedValueOnce(connectionMock);
 
     const db = await connectionMock.db('TodoList');
     const users = await db.collection('users');
+    const tasks = await db.collection('tasks');
     await users.deleteMany({});
+    await tasks.deleteMany({});
 
     const req = request(app);
     await req
@@ -33,19 +36,32 @@ describe('Post /task', () => {
         password: '123456',
       });
     authorization = body.token;
+
+    await req
+      .post('/task')
+      .set({ authorization })
+      .send({
+        title: 'Tarefa teste',
+        description: 'Descrição da tarefa teste',
+        priority: '2',
+        status: 'inProcess',
+      });
+
+    const { _id } = await tasks.findOne({ title: 'Tarefa teste' });
+    taskId = _id.toString();
   });
 
   afterAll(() => {
     jest.restoreAllMocks();
   });
 
-  it('verifica a resposta ao postar uma task faltando title', async () => {
+  it('verifica a resposta ao modificar uma task faltando title', async () => {
     const req = request(app);
     const response = await req
-      .post('/task')
+      .put(`/task/${taskId}`)
       .set({ authorization })
       .send({
-        description: 'Descrição da tarefa 3',
+        description: 'Descrição da tarefa teste',
         priority: '2',
         status: 'inProcess',
       });
@@ -55,13 +71,13 @@ describe('Post /task', () => {
     expect(response.body.message).toBe('Invalid entries. Try again.');
   });
 
-  it('verifica a resposta ao postar uma task faltando description', async () => {
+  it('verifica a resposta ao modificar uma task faltando description', async () => {
     const req = request(app);
     const response = await req
-      .post('/task')
+      .put(`/task/${taskId}`)
       .set({ authorization })
       .send({
-        title: 'Tarefa 3',
+        title: 'Tarefa Modificada',
         priority: '2',
         status: 'inProcess',
       });
@@ -71,14 +87,14 @@ describe('Post /task', () => {
     expect(response.body.message).toBe('Invalid entries. Try again.');
   });
 
-  it('verifica a resposta ao postar uma task faltando priority', async () => {
+  it('verifica a resposta ao modificar uma task faltando priority', async () => {
     const req = request(app);
     const response = await req
-      .post('/task')
+      .put(`/task/${taskId}`)
       .set({ authorization })
       .send({
-        title: 'Tarefa 3',
-        description: 'Descrição da tarefa 3',
+        title: 'Tarefa Modificada',
+        description: 'Descrição da tarefa teste',
         status: 'inProcess',
       });
 
@@ -87,14 +103,14 @@ describe('Post /task', () => {
     expect(response.body.message).toBe('Invalid entries. Try again.');
   });
 
-  it('verifica a resposta ao postar uma task faltando status', async () => {
+  it('verifica a resposta ao modificar uma task faltando status', async () => {
     const req = request(app);
     const response = await req
-      .post('/task')
+      .put(`/task/${taskId}`)
       .set({ authorization })
       .send({
-        title: 'Tarefa 3',
-        description: 'Descrição da tarefa 3',
+        title: 'Tarefa Modificada',
+        description: 'Descrição da tarefa teste',
         priority: '2',
       });
 
@@ -103,13 +119,13 @@ describe('Post /task', () => {
     expect(response.body.message).toBe('Invalid entries. Try again.');
   });
 
-  it('verifica a resposta ao postar uma task sem token', async () => {
+  it('verifica a resposta ao tentar modificar uma task sem o token', async () => {
     const req = request(app);
     const response = await req
-      .post('/task')
+      .put(`/task/${taskId}`)
       .send({
-        title: 'Tarefa 3',
-        description: 'Descrição da tarefa 3',
+        title: 'Tarefa Modificada',
+        description: 'Descrição da tarefa teste',
         priority: '2',
         status: 'inProcess',
       });
@@ -119,20 +135,26 @@ describe('Post /task', () => {
     expect(response.body.message).toBe('missing auth token');
   });
 
-  it('verifica a resposta quando uma task é postada com sucesso', async () => {
+  it('verifica a resposta ao tentar buscar tasks corretamente', async () => {
     const req = request(app);
-    const response = await req
-      .post('/task')
+    const task = await req
+      .put(`/task/${taskId}`)
       .set({ authorization })
       .send({
-        title: 'Tarefa 3',
-        description: 'Descrição da tarefa 3',
+        title: 'Tarefa Modificada',
+        description: 'Descrição da tarefa teste',
         priority: '2',
         status: 'inProcess',
       });
 
-    expect(response.statusCode).toBe(201);
-    expect(response.body).toHaveProperty('message');
-    expect(response.body.message).toBe('Task created successfully');
+    expect(task.body).toHaveProperty('title');
+    expect(task.body).toHaveProperty('status');
+    expect(task.body).toHaveProperty('priority');
+    expect(task.body).toHaveProperty('created');
+    expect(task.body).toHaveProperty('_id');
+    expect(task.body.title).toBe('Tarefa Modificada');
+    expect(task.body.status).toBe('inProcess');
+    expect(task.body.priority).toBe('2');
+    expect(task.body._id).toBe(taskId);
   });
 });
